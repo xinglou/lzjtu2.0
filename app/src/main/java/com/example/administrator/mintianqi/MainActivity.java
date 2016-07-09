a1afa30d70c816d5ed3595f5d36f7a3cb147e5b2
xinglou <xinglou1@foxmail.com>
2016-07-09 15:46:46 +0800

    GetNet

--- app/src/main/java/com/example/administrator/mintianqi/MainActivity.java
+++ app/src/main/java/com/example/administrator/mintianqi/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.administrator.mintianqi;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import cn.edu.pku.zhangqixun.util.NetUtil;
 
 /**
  * Created by Administrator on 2016/7/8.
@@ -11,5 +15,12 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
+            Log.d("myWeather", "网络OK");
+            Toast.makeText(MainActivity.this, "网络OK！", Toast.LENGTH_LONG).show();
+        } else {
+            Log.d("myWeather", "网络挂了");
+            Toast.makeText(MainActivity.this, "网络挂了！", Toast.LENGTH_LONG).show();
+        }
     }
 }
