500bc3d31acfd700421f26ab28cc49ab2a45b67c
xinglou <xinglou1@foxmail.com>
2016-07-09 16:38:17 +0800

    edit net

--- app/src/main/java/com/example/administrator/mintianqi/MainActivity.java
+++ app/src/main/java/com/example/administrator/mintianqi/MainActivity.java
@@ -1,26 +1,102 @@
 package com.example.administrator.mintianqi;
-
-
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import cn.edu.pku.zhangqixun.util.NetUtil;
 
 /**
- * Created by Administrator on 2016/7/8.
+ * Created by zhangqixun on 16/7/4.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private ImageView mUpdateBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+
+        mUpdateBtn = (ImageView) findViewById(R.id.title_update_btn);
+        mUpdateBtn.setOnClickListener(this);
+
         if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
             Log.d("myWeather", "网络OK");
-            Toast.makeText(MainActivity.this, "网络OK！", Toast.LENGTH_LONG).show();
-        } else {
+            Toast.makeText(MainActivity.this,"网络OK！", Toast.LENGTH_LONG).show();
+        }else
+        {
             Log.d("myWeather", "网络挂了");
-            Toast.makeText(MainActivity.this, "网络挂了！", Toast.LENGTH_LONG).show();
+            Toast.makeText(MainActivity.this,"网络挂了！", Toast.LENGTH_LONG).show();
         }
     }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view.getId() == R.id.title_update_btn){
+
+            SharedPreferences sharedPreferences = getSharedPreferences("config", MODE_PRIVATE);
+            String cityCode = sharedPreferences.getString("main_city_code","101160101");
+            Toast.makeText(MainActivity.this,"cityCode:" + cityCode,Toast.LENGTH_LONG).show();
+
+
+            if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
+                Toast.makeText(MainActivity.this,"网络OK！" + cityCode,Toast.LENGTH_LONG).show();
+                queryWeatherCode(cityCode);
+            }else
+            {
+                Log.d("myWeather", "网络挂了");
+                Toast.makeText(MainActivity.this,"网络挂了！",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    /**
+     *
+     * @param cityCode
+     */
+    private void queryWeatherCode(String cityCode)  {
+        final String address = "http://wthrcdn.etouch.cn/WeatherApi?citykey=" + cityCode;
+        Log.d("myWeather", address);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection con=null;
+                try{
+                    URL url = new URL(address);
+                    con = (HttpURLConnection)url.openConnection();
+                    con.setRequestMethod("GET");
+                    con.setConnectTimeout(8000);
+                    con.setReadTimeout(8000);
+                    InputStream in = con.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String str;
+                    while((str=reader.readLine()) != null){
+                        response.append(str);
+                        Log.d("myWeather", str);
+                    }
+                    String responseStr=response.toString();
+                    Log.d("myWeather", responseStr);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    if(con != null){
+                        con.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+
 }
