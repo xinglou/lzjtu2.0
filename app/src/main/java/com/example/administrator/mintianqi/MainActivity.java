00fc67b4fb8a4ee743064120d704fac34817a015
xinglou <xinglou1@foxmail.com>
2016-07-09 12:28:00 +0800

    first commit

---
+++ app/src/main/java/com/example/administrator/mintianqi/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.mintianqi;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/7/8.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
